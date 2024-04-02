315293c74a2ebb2757f2adb743fa63f815096337
Joan Plaja <joan.plaja@fundcraft.lu>
2024-04-02 12:22:52 +0200

    Restructure cheat command and add a command for tracing linux processes

--- bin/cheatsheet.sh
+++ bin/cheatsheet.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-# Function to display git cheat sheet
 git_cheatsheet() {
 	echo "git reset --hard (HEAD~1|origin/develop) - Reset to (last commit|Origin branch)"
 	echo "git revert (HEAD|(HEAD^|HEAD~N)..HEAD) - Revert (last commit|(last 2 commits|last N commits))"
 }
 
-# Function to display network commands cheat sheet
-network_cheatsheet() {
-	echo "ifconfig - Show network interface configuration"
-	echo "ping <host> - Send ICMP echo request to a host"
-	echo "netstat - Display network connections, routing tables, etc."
-	echo "traceroute <host> - Trace route to a host"
-}
-
-# Function to display grep commands cheat sheet
-grep_cheatsheet() {
-	echo "grep 'pattern' <file> - Search for a pattern in a file"
-	echo "grep -r 'pattern' <directory> - Recursively search for a pattern in a directory"
-	echo "grep -i 'pattern' <file> - Perform case-insensitive search"
-	echo "grep -v 'pattern' <file> - Invert match, display lines that do not match the pattern"
-}
-
-# Function to display redis commands cheat sheet
 redis_cheatsheet() {
 	echo "redis-cli -h HOST_NAME - Connecting with cli to redis"
 	echo "MONITOR - Realtime information about operations"
@@ -32,23 +14,48 @@ redis_cheatsheet() {
 	echo "FLUSHDB - Delete all keys on the current database"
 }
 
+linux_cheatsheet() {
+	case "$1" in
+	debug)
+		echo "strace (https://strace.io/)"
+		echo "It is used to monitor and tamper with interactions between processes and the Linux kernel, which include system calls, signal deliveries, and changes of process state."
+		echo "strace -p 26380 -- Attach to already running process"
+		echo "strace -yy cat /dev/null -- Print paths and more info associated with file descriptors."
+		echo "strace -c ls > /dev/null -- Count time, calls, and errors for each system call."
+		;;
+	grep)
+		echo "grep 'pattern' <file> - Search for a pattern in a file"
+		echo "grep -r 'pattern' <directory> - Recursively search for a pattern in a directory"
+		echo "grep -i 'pattern' <file> - Perform case-insensitive search"
+		echo "grep -v 'pattern' <file> - Invert match, display lines that do not match the pattern"
+		;;
+	network)
+		echo "ifconfig - Show network interface configuration"
+		echo "ping <host> - Send ICMP echo request to a host"
+		echo "netstat - Display network connections, routing tables, etc."
+		echo "traceroute <host> - Trace route to a host"
+		;;
+	*)
+		echo "Usage: cheat linux {debug|grep|network}"
+		;;
+	esac
+
+}
+
 # Main function
 cheat() {
 	case "$1" in
+	linux)
+		linux_cheatsheet $2
+		;;
 	git)
 		git_cheatsheet
-		;;
-	network)
-		network_cheatsheet
-		;;
-	grep)
-		grep_cheatsheet
 		;;
 	redis)
 		redis_cheatsheet
 		;;
 	*)
-		echo "Usage: $0 {git|network|grep}"
+		echo "Usage: $0 {linux{debug|grep|network}|git|redis}"
 		;;
 	esac
 }
